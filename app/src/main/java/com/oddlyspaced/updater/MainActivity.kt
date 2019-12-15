622d85efdcb7f62da7be521336a2cd6aba627036
oddlyspaced <oddlyspaced1@gmail.com>
2019-12-16 00:26:15 +0530

    Apply dark mode along with transparent status bar

--- app/src/main/java/com/oddlyspaced/updater/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/updater/MainActivity.kt
@@ -1,24 +1,58 @@
 package com.oddlyspaced.updater
 
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val getUpdate by lazy {GetUpdate(getString(R.string.updater_link))}
-    private val getProp by lazy {GetProp()}
+    private val getUpdate by lazy { GetUpdate(getString(R.string.updater_link)) }
+    private val getProp by lazy { GetProp() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        makeStatusBarTransparent()
         if (checkSupport() == 1) {
             loadDetails()
             setOnClick()
         }
+    }
+
+    // https://stackoverflow.com/questions/44170028/android-how-to-detect-if-night-mode-is-on-when-using-appcompatdelegate-mode-ni
+    // https://learnpainless.com/android/material/make-fully-android-transparent-status-bar
+    private fun makeStatusBarTransparent() {
+        val mode = applicationContext?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)
+        when (mode) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+                window.statusBarColor = Color.TRANSPARENT
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+                window.statusBarColor = Color.TRANSPARENT
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
+        }
+    }
+
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
     }
 
     private fun checkSupport(): Int {
@@ -49,8 +83,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }, 100
             )
-        }
-        else {
+        } else {
             txCheck.text = getString(R.string.button_unable_check)
             pbCheck.visibility = View.INVISIBLE
             txCheck.visibility = View.VISIBLE
@@ -58,12 +91,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadDetails() {
-        val versionString = getString(R.string.body_android_version) + getProp.getProp("ro.build.version.release").trim()
-        val versionCosmic = getString(R.string.body_cosmic_version) + getProp.getProp("ro.cos.version").trim()
+        val versionString =
+            getString(R.string.body_android_version) + getProp.getProp("ro.build.version.release").trim()
+        val versionCosmic =
+            getString(R.string.body_cosmic_version) + getProp.getProp("ro.cos.version").trim()
         txMain1.text = versionString
         txMain2_1.text = versionCosmic
         txMain2_2.text = dateCodeToString(getProp.getProp("ro.cos.builddate").trim())
-        txMain3_2.text = securityPatchToString(getProp.getProp("ro.build.version.security_patch").trim())
+        txMain3_2.text =
+            securityPatchToString(getProp.getProp("ro.build.version.security_patch").trim())
     }
 
     private fun setOnClick() {
@@ -96,7 +132,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun monthToString(month: Int): String {
-        return when(month) {
+        return when (month) {
             1 -> getString(R.string.month_january)
             2 -> getString(R.string.month_february)
             3 -> getString(R.string.month_march)
@@ -118,13 +154,13 @@ class MainActivity : AppCompatActivity() {
             consHolder.visibility = View.GONE
             txHeader.text = getString(R.string.header_update_unavailable)
             cardCheck.visibility = View.GONE
-        }
-        else {
+        } else {
             imgMain1.setImageDrawable(getDrawable(R.drawable.ic_download))
             imgMain2.setImageDrawable(getDrawable(R.drawable.ic_calendar))
             imgMain3.setImageDrawable(getDrawable(R.drawable.ic_changelog))
             txHeader.text = getString(R.string.header_update_available)
-            txMain1.text = getString(R.string.body_size_prefix) + modal.size + getString(R.string.body_size_suffix)
+            txMain1.text =
+                getString(R.string.body_size_prefix) + modal.size + getString(R.string.body_size_suffix)
             txMain2_1.text = dateCodeToString(modal.build).trim()
             txMain2_2.visibility = View.GONE
             txMain3_1.text = getString(R.string.body_changelog)
